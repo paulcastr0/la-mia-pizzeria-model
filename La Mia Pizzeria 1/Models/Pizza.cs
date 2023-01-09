138d9e6d583ba893d5f0d18548a2e6707384b079
paulcastr0 <castropaul.learn@gmail.com>
2023-01-09 17:59:49 +0100

    Completamento Esercizio

---
+++ La Mia Pizzeria 1/Models/Pizza.cs
@@ -0,0 +1,25 @@
+﻿using System.Transactions;
+
+namespace La_Mia_Pizzeria_1.Models
+{
+    public class Pizza
+    {
+        //Proprietà Pizza
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+
+        //Costruttore Pizza
+        public Pizza(int id,string image, string name, string description,float price)
+        {
+            Id = id;
+            Image = image;
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+
+    }
+}
---
+++ La Mia Pizzeria 1/Utils/PizzaData.cs
@@ -0,0 +1,33 @@
+﻿using La_Mia_Pizzeria_1.Models;
+using Microsoft.Extensions.Hosting;
+using System;
+
+
+
+
+namespace La_Mia_Pizzeria_1.Utils
+{
+    //Classe che rappresenta lista delle pizze
+    public class PizzaData
+    {
+        private static List<Pizza> pizzas;
+
+        public static List<Pizza>GetPizzas()
+        {
+            if(pizzas != null )
+            {
+                return pizzas;
+            }
+
+            pizzas = new List<Pizza>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Pizza postGenerato = new Pizza(i, "https://picsum.photos/id/", "Titolo post: " + i, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", i);
+                pizzas.Add(postGenerato);
+            }
+            return pizzas;
+        }
+
+    }
+}
